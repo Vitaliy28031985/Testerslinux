284bbd15df8933cfb632b553df53c7e52814177d
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-05-11 20:40:15 +0300

    markap

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 import './sass/main.scss';
 
+import {testAry} from "/js/testary";
+
+
+const inputConteiner = document.querySelector('.Linux-testers');
+
+
+const createInput = () => {
+   const inputListElements = testAry.map(
+       ({ name, a, b, c, d, i, }) =>
+         `
+         <p class="question">${name}</p>
+         <div class="test">
+      
+         <input class="input" name="${a.id}" value="${a.id}" id="${a.id}" type="checkbox"><label class="label" for="${a.id}">${a.answer}</label>
+      
+         <input class="input" name="${b.id}" value="${b.id}" id="${b.id}" type="checkbox"><label class="label" for="${b.id}">${b.answer}</label>
+      
+         <input class="input" name="${c.id}" value="${c.id}" id="${c.id}" type="checkbox"><label class="label" for="${c.id}">${c.answer}</label>
+      
+          <input class="input" name="${d.id}" value="${d.id}" id="${d.id}" type="checkbox"><label class="label" for="${d.id}">${d.answer}</label>
+      
+          <input class="input" name="${i.id}" value="${i.id}" id="${i.id}" type="checkbox"><label class="label" for="${i.id}">${i.answer}</label>
+
+         </div>`
+     )
+     .join("");
+ 
+     inputConteiner.insertAdjacentHTML("afterbegin", inputListElements);
+ };
+ createInput();
---
+++ src/js/testary.js
@@ -0,0 +1,29 @@
+export const testAry = [
+
+   {name: '1. Якою командою в ОС Лінукс відкривають директорію?',
+   a: {answer:'a. ls', id:'1-ls',},
+   b: {answer:'cd', id:'1-cd',},
+   c: {answer:'open', id:'1-open',},
+   d: {answer:'mkdir', id:'1-mkdir',},
+   i: {answer:'dir', id:'dir',},
+   answerCorrect: 'cd',
+},
+
+  {name: '2. Якою командою в ОС Лінукс можна повернутися у батьківську директорію?',
+   a: {answer:'cd', id:'2-cd',},
+   b: {answer:'ls -lh', id:'2-lh',},
+   c: {answer:'echo -e', id:'2-echo',},
+   d: {answer:'cat -A', id:'2-cat',},
+   i: {answer:'dir', id:'2-dir'},
+   answerCorrect: 'cd',
+},
+
+{name: '3. Як називається початок скрипта, написаного на мові сценаріїв bash, у вигляді #!/bin/bash?',
+   a: {answer:'cowabunga', id:'3-cowabunga',},
+   b: {answer:'fenshui', id:'3-fenshui',},
+   c: {answer:'supercode', id:'3-supercode',},
+   d: {answer:'bigbang', id:'3-bigbang',},
+   i: {answer:'shebang', id:'shebang',},
+   answerCorrect: 'shebang',
+},
+]
