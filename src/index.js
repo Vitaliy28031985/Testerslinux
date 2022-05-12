fc414d89b06ea44b7db9b896c61c86ff0e54d9e2
mac <94307584+Vitaliy28031985@users.noreply.github.com>
2022-05-12 09:48:21 +0300

    radio

--- src/index.js
+++ src/index.js
@@ -16,15 +16,15 @@ const createInput = () => {
          <p class="question">${name}</p>
          <div class="test">
       
-         <input class="input" data-corrects="${answerCorrect}" name="${a.id}" value="${a.answer}" id="${a.id}" type="checkbox"><label class="label" for="${a.id}">${a.answer}</label>
+         <input class="input" data-corrects="${answerCorrect}" name="${a.id}" value="${a.answer}" id="${a.id}" type="radio"><label class="label" for="${a.id}">${a.answer}</label>
       
-         <input class="input" data-corrects="${answerCorrect}" name="${b.id}" value="${b.answer}" id="${b.id}" type="checkbox"><label class="label" for="${b.id}">${b.answer}</label>
+         <input class="input" data-corrects="${answerCorrect}" name="${b.id}" value="${b.answer}" id="${b.id}" type="radio"><label class="label" for="${b.id}">${b.answer}</label>
       
-         <input class="input" data-corrects="${answerCorrect}" name="${c.id}" value="${c.answer}" id="${c.id}" type="checkbox"><label class="label" for="${c.id}">${c.answer}</label>
+         <input class="input" data-corrects="${answerCorrect}" name="${c.id}" value="${c.answer}" id="${c.id}" type="radio"><label class="label" for="${c.id}">${c.answer}</label>
       
-          <input class="input" data-corrects="${answerCorrect}" name="${d.id}" value="${d.answer}" id="${d.id}" type="checkbox"><label class="label" for="${d.id}">${d.answer}</label>
+          <input class="input" data-corrects="${answerCorrect}" name="${d.id}" value="${d.answer}" id="${d.id}" type="radio"><label class="label" for="${d.id}">${d.answer}</label>
       
-          <input class="input" data-corrects="${answerCorrect}" name="${i.id}" value="${i.answer}" id="${i.id}" type="checkbox"><label class="label" for="${i.id}">${i.answer}</label>
+          <input class="input" data-corrects="${answerCorrect}" name="${i.id}" value="${i.answer}" id="${i.id}" type="radio"><label class="label" for="${i.id}">${i.answer}</label>
 
          </div>`
      )
